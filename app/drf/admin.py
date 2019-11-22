7476903bba23e1d684361d179162d3344cd9af1c
Ayanna Bowers-Shaw <geekwise.ayanna.bs@gmail.com>
2019-11-22 23:27:53 +0000

    registered models inside of admin.py

--- app/drf/admin.py
+++ app/drf/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from drf.account import Account
+from drf.product import Product
+from drf.customer import Customer
 
 # Register your models here.
+admin.site.Register(Account)
+admin.site.Register(Product)
+admin.site.Register(Customer)
